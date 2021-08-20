39f978f6eef8e7f993cfdd091112d9cc3b56b47d
RajatRaikar1 <88779767+RajatRaikar1@users.noreply.github.com>
2021-08-20 11:42:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const path = require('path')
+const hbs = require('hbs')
+
+const port = process.env.PORT || 3000
+
+
+
+const app = express()
+
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+
+app.use(express.static(publicDirectoryPath))
+app.set('view engine', 'hbs')
+
+
+
+const viewsPath = path.join(__dirname, '../public')
+
+app.set('views', viewsPath)
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+})
+
+app.post('/register', (req, res) => {
+    res.send('okay:'+ req.body.uname)
+})
+
+
+app.listen(port, () => {
+    console.log('server starting...' + port) 
+})
